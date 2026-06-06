1ef817db56f185a5849ffcf4281d6e06110b0ae1
Vishnu Sharma <vishsharma8445@gmail.com>
2026-06-06 18:30:50 +0530

    Maximum Subarray.cpp

--- Week1/Day2/Maximum Subarray.cpp
+++ Week1/Day2/Maximum Subarray.cpp
@@ -1,1 +1,17 @@
-
+class Solution {
+public:
+    int maxSubArray(vector<int>& arr) {
+        int maxi =INT_MIN;
+        int prefix=0;
+        int n=arr.size();
+        for(int i=0;i<n;i++)
+        {
+            prefix+=arr[i];
+            maxi=max(maxi,prefix);
+            if(prefix<0)
+             prefix=0;
+        }
+        return maxi;
+    }
+};
+    
