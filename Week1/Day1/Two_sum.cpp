6c90588f3077c3e843cacdbc3f7a113498f28f05
Vishnu Sharma <vishsharma8445@gmail.com>
2026-06-06 17:33:40 +0530

    Update Two_sum.cpp

--- Week1/Day1/Two_sum.cpp
+++ Week1/Day1/Two_sum.cpp
@@ -1,1 +1,15 @@
-
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int>st; vector<int>ans;
+        st[nums[0]]=0; int i;
+        for(i=1;i<nums.size();i++)
+        {
+             if(st.find(target-nums[i])!=st.end())
+                  break;
+            st[nums[i]]=i;
+        }
+        ans.push_back(i); ans.push_back(st[target-nums[i]]);
+        return ans;
+    }
+};
