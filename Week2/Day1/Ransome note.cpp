0b3046ae784fcbacda338faccd74ee40248ed9da
Vishnu Sharma <vishsharma8445@gmail.com>
2026-06-07 23:56:31 +0530

    Create Ransome note.cpp

---
+++ Week2/Day1/Ransome note.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        int freq[26] = {0};
+
+        for(char c : magazine)
+            freq[c - 'a']++;
+
+        for(char c : ransomNote)
+        {
+            if(freq[c - 'a'] == 0)
+                return false;
+            freq[c - 'a']--;
+        }
+
+        return true;
+    }
+};
