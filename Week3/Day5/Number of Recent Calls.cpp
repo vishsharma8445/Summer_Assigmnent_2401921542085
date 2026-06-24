b08189f0f59584c5f4114ba9c294850b2cfc829c
Vishnu Sharma <vishsharma8445@gmail.com>
2026-06-24 18:26:06 +0530

    Create Number of Recent Calls.cpp

---
+++ Week3/Day5/Number of Recent Calls.cpp
@@ -0,0 +1,18 @@
+class RecentCounter {
+public:
+    queue<int> q;
+
+    RecentCounter() {
+        
+    }
+    
+    int ping(int t) {
+        q.push(t);
+
+        while (!q.empty() && q.front() < t - 3000) {
+            q.pop();
+        }
+
+        return q.size();
+    }
+};
